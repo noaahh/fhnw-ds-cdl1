c53bf148f5859ac965028fe91ee9587d5364f5c4
Noah <thenoahleuenberger@gmail.com>
2024-05-09 13:15:18 +0200

    Added reusable sklearn train function

--- sklearn_mvp.py
+++ sklearn_mvp.py
@@ -1,3 +1,5 @@
+import numpy as np
+from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score
 from tqdm import tqdm
@@ -5,32 +7,37 @@ from tqdm import tqdm
 from src.data.dataset import SensorDatasetSKLearn
 from src.data.partition_helper import get_partitioned_data, get_partition_paths
 
-partitions_paths = get_partition_paths("./data/splits", k_folds=5)
-data = get_partitioned_data(partitions_paths)
 
-evaluation_results = []
+def train_and_evaluate_model(data_partitions, model: BaseEstimator, metrics: dict):
+    evaluation_results = []
+    for fold_idx, fold in tqdm(enumerate(data_partitions), desc="Processing folds", unit="fold"):
+        train_data, validate_data = fold['train'], fold['validate']
 
-for fold_idx, fold in tqdm(enumerate(data), desc="Processing folds", unit="fold"):
-    train_data, validate_data = fold['train'], fold['validate']
-    train_dataset = SensorDatasetSKLearn(train_data)
-    validate_dataset = SensorDatasetSKLearn(validate_data)
+        train_dataset = SensorDatasetSKLearn(train_data)
+        validate_dataset = SensorDatasetSKLearn(validate_data)
+        X_train, y_train = train_dataset.get_data()
+        X_validate, y_validate = validate_dataset.get_data()
 
-    X_train, y_train = train_dataset.get_data()
-    X_validate, y_validate = validate_dataset.get_data()
+        model.fit(X_train, y_train)
+        predictions = model.predict(X_validate)
 
-    model = LogisticRegression(random_state=1337, max_iter=1000)
-    model.fit(X_train, y_train)
+        results = {name: metric(y_validate, predictions) for name, metric in metrics.items()}
+        evaluation_results.append(results)
 
-    predictions = model.predict(X_validate)
-    accuracy = accuracy_score(y_validate, predictions)
-    f1 = f1_score(y_validate, predictions, average='macro')
+        fold_results = ", ".join([f"{name}: {score:.2f}" for name, score in results.items()])
+        print(f"Fold {fold_idx + 1} {fold_results}")
 
-    evaluation_results.append({
-        'accuracy': accuracy,
-        'f1': f1
-    })
-    print(f"Fold {fold_idx + 1} accuracy: {accuracy:.2f}, f1: {f1:.2f}")
+    average_scores = {name: np.mean([result[name] for result in evaluation_results]) for name in metrics.keys()}
+    avg_results = ", ".join([f"Average {name}: {score:.2f}" for name, score in average_scores.items()])
+    print(avg_results)
 
-average_accuracy = sum([r['accuracy'] for r in evaluation_results]) / len(evaluation_results)
-average_f1 = sum([r['f1'] for r in evaluation_results]) / len(evaluation_results)
-print(f"Average accuracy: {average_accuracy:.2f}, average f1: {average_f1:.2f}")
+
+data = get_partitioned_data(get_partition_paths("./data/splits", k_folds=5))
+
+model = LogisticRegression(random_state=1337, max_iter=1000)
+metrics = {
+    'accuracy': accuracy_score,
+    'f1': lambda y, pred: f1_score(y, pred, average='macro')
+}
+
+train_and_evaluate_model(data, model, metrics)
