046cfd3c5ed9bd9b07eb113cf86a72655e775501
Nils <nils.fahrni@students.fhnw.ch>
2024-06-06 17:51:42 +0200

    added simple cnn model

---
+++ src/models/cnn.py
@@ -0,0 +1,56 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Simple1DCNN(pl.LightningModule):
+    def __init__(self, input_length, input_channels, num_classes, learning_rate=1e-3):
+        super(Simple1DCNN, self).__init__()
+        self.save_hyperparameters()
+
+        self.conv1 = nn.Conv1d(input_channels, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv1d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=1)
+        
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+        
+        self.fc1 = nn.Linear(64 * (input_length // 8), 128)
+        self.fc2 = nn.Linear(128, num_classes)
+        
+        self.dropout = nn.Dropout(0.5)
+        
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = nn.CrossEntropyLoss()(y_hat, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = nn.CrossEntropyLoss()(y_hat, y)
+        self.log('val_loss', loss)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = nn.CrossEntropyLoss()(y_hat, y)
+        self.log('test_loss', loss)
+        return loss
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        return optimizer
